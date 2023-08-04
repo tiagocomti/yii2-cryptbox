5ebab8cd5f3ba9e39f8c84b2385ecc6596261b8f
tiago <tiago.alexandre.oliveira@hotmail.com>
2023-08-04 09:58:19 -0300

    Criando a possibilidade de enviar um item/arquivo para o s3 de forma criptografada

--- Hsm.php
+++ Hsm.php
@@ -3,6 +3,7 @@
 namespace tiagocomti\cryptbox;
 
 use tiagocomti\cryptbox\models\api\AwsS3;
+use tiagocomti\cryptbox\models\api\responses\aws\S3Setup;
 use yii\db\Exception;
 use yii;
 
@@ -30,6 +31,38 @@ class Hsm {
             $photo = AwsS3::uploadFileS3('agf-sec','hsm/keys/',$this->secretName,Cryptbox::easyEncrypt($file,Cryptbox::getOurSecret()));
             return true;
         }
+    }
+
+    public function uploadItem($item_name, $content){
+        if($this->provider === self::PROVIDER_AWS_S3){
+            $s3 = new AwsS3();
+
+            $resposta = AwsS3::uploadFileS3('agf-sec','hsm/keys/',$item_name,Cryptbox::easyEncrypt($content,Cryptbox::getOurSecret()));
+
+            return $resposta;
+        }
+    }
+
+    public function getMyItem($item_name){
+        if(Yii::$app->cryptbox->enableCache === true) {
+            if (Yii::$app->cache->get($item_name) === false) {
+                $crend = AwsS3::getCredentialsByFile($this->crendPath.'/'.$this->crendJson);
+                $key = AwsS3::getAuthenticatedIten($crend, "hsm/keys/" . $item_name);
+                if(empty($key)){
+                    throw new \Exception("Falha ao localizar chave ". $item_name." no bucket");
+                }
+                $key = Cryptbox::easyDecrypt($key, Cryptbox::getOurSecret());
+                Yii::$app->cache->set($item_name, $key, (Yii::$app->cryptbox->timeCache) ?: 600000);
+            }else{
+                $key = Yii::$app->cache->get($item_name);
+            }
+        }else {
+            $crend = AwsS3::getCredentialsByFile($this->crendPath . '/' . $this->crendJson);
+            $key = AwsS3::getAuthenticatedIten($crend, "hsm/keys/" .$item_name);
+            $key = Cryptbox::easyDecrypt($key, Cryptbox::getOurSecret());
+        }
+
+        return $key;
     }
 
     public function updateMyKey(){
--- commands/controllers/HsmController.php
+++ commands/controllers/HsmController.php
@@ -12,6 +12,8 @@ use yii\console\ExitCode;
 use yii\db\ActiveRecord;
 use yii\db\Exception;
 use yii\helpers\BaseConsole;
+use yii\helpers\Console;
+
 class HsmController extends Controller
 {
     public function actionUploadMyKey($path){
@@ -21,4 +23,22 @@ class HsmController extends Controller
     public function actionUpdateMyKey(){
         \Yii::$app->hsm->updateMyKey();
     }
+
+    public function actionStorageItem($itemName, $path){
+        $file = fopen($path, 'rb');
+        if(is_resource($file)) {
+            $file = (stream_get_contents($file));
+        }
+        $upload = \Yii::$app->hsm->uploadItem($itemName, $file);
+        BaseConsole::output($this->ansiFormat($upload, Console::FG_GREEN));
+    }
+
+    public function actionGetItem($itemName, $pathToSave){
+        $key = \Yii::$app->hsm->getMyItem($itemName);
+        $myfile = fopen($pathToSave, "w+");
+        chown($pathToSave,"www");
+        fwrite($myfile, $key);
+        fclose($myfile);
+    }
+
 }
